cddbdaec0f90df934fdf21a04e59b34349e5e032
Chelsea Chaussee <cfchaussee@gmail.com>
2021-03-22 18:19:34 -0700

    html/flask is not my friend

--- app.py
+++ app.py
@@ -18,6 +18,71 @@ def home():
     pleasework = db.mars_facts.find()
     return render_template ("index.html", pleasework = pleasework)
 
-if __name__ == '__main__'
+def featured_image(browser):
+
+    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url)
+
+
+    full_image_button = browser.find_by_id("full_image")
+    full_image_button.click()
+
+    browser.is_element_present_by_text("more info", wait_time=1)
+    more_info_element = browser.find_link_by_partial_text("more info")
+    more_info_element.click()
+
+    html = browser.html
+    image_soup = BeautifulSoup(html, "html.parser")
+
+    img = image_soup.select_one("figure.lede a img")
+    try:
+        img_url = img.get("src")
+    except AttributeError:
+        return None 
+
+    img_url = f"https://www.jpl.nasa.gov{img_url}"
+    return img_url
+
+mars_facts = pd.read_html("https://space-facts.com/mars/")[0]
+print(mars_facts)
+mars_facts.reset_index(inplace=True)
+mars_facts.columns=["ID", "Properties", "Mars", "Earth"]
+mars_facts
+
+def scrape_hemisphere(html_text):
+    hemisphere_soup = BeautifulSoup(html_text, "html.parser")
+    try: 
+        title_element = hemisphere_soup.find("h2", class_="title").get_text()
+        sample_element = hemisphere_soup.find("a", text="Sample").get("href")
+    except AttributeError:
+        title_element = None
+        sample_element = None 
+    hemisphere = {
+        "title": title_element,
+        "img_url": sample_element
+    }
+    return hemisphere
+
+def scrape_all():
+    executable_path = {"executable_path": "/Users/jorgesanchez/Downloads/chromedriver"}
+    browser = Browser("chrome", **executable_path, headless=False)
+    news_title, news_paragraph = mars_news(browser)
+    img_url = featured_image(browser)
+    mars_weather = twitter_weather(browser)
+    facts = mars_facts
+    hemisphere_image_urls = hemisphere(browser)
+    timestamp = dt.datetime.now()
+
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": img_url,
+        "facts": facts,
+        "hemispheres": hemisphere_image_urls,
+        "last_modified": timestamp
+    }
+    browser.quit()
+    return data 
+if __name__ == '__main__':
     app.run(debug =True)
     
